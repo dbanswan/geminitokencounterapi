b0da68aaef061d20ee98d8f917616fba09ce6dc3
varnandh <dbanswan13@gmail.com>
2024-04-20 21:24:28 +0530

    commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
 
+google_api_key = os.getenv("API_KEY")
+# print(google_api_key)
+genai.configure(api_key=google_api_key)
 # handle cors
 
 
@@ -19,18 +25,28 @@ cl100k_base = [
 ]
 p50k_base = ["Codex models", "text-davinci-002", "text-davinci-003", "gpt-2"]
 r50k_base = ["davinci", "curie", "babbage", "ada", "text-davinci-001"]
+gemini_models = [
+    "gemini-1.0-pro",
+    "gemini-1.0-pro-001",
+    "gemini-1.0-pro-latest",
+    "gemini-1.0-pro-vision-latest",
+    "gemini-1.5-pro-latest",
+    "gemini-pro",
+    "gemini-pro-vision",
+]
+
 app = Flask(__name__)
 # make sure we only access api from tokencounter.dbanswan.com
 CORS(
     app,
     resources={
         r"/*": {
-            "origins": "https://tokencounter.dbanswan.com",
+            "origins": ["https://tokencounter.dbanswan.com", "http://localhost:3000"],
             "methods": ["GET", "POST"],
         }
     },
 )
-# CORS(app)
+CORS(app)
 
 
 @app.route("/tokenize", methods=["GET"])
@@ -42,34 +58,55 @@ def get_tokens():
 def count_tokens():
 
     encoding = tiktoken.get_encoding("cl100k_base")
+    num_tokens = 0
     data = request.json
     text = data["text"]
     model = data["model"]
     encoding = ""
-    if model in cl100k_base:
+    if model in gemini_models:
+        # google_api_key = os.getenv("API_KEY")
+        # print(google_api_key)
+        # genai.configure(api_key=google_api_key)
+        model = "models/" + model
+        gemini_model = genai.GenerativeModel(model)
+        num_tokens = gemini_model.count_tokens(text).total_tokens
+        # print(type(num_tokens))
+
+        # num_tokens = num_tokens.split("total_tokens: ")[1]
+        print(num_tokens)
+
+    elif model in cl100k_base:
         encoding = tiktoken.get_encoding("cl100k_base")
+        num_tokens = len(encoding.encode(text))
+
     elif model in p50k_base:
         encoding = tiktoken.get_encoding("p50k_base")
+        num_tokens = len(encoding.encode(text))
     elif model in r50k_base:
         encoding = tiktoken.get_encoding("r50k_base")
+        num_tokens = len(encoding.encode(text))
     else:
         # send a 404 error with object {"error": true, "message": "Model not found in the list of models"} convert to json
         return {
             "error": True,
             "message": "Model not found in the list of models",
-            data: None,
+            "data": -1,
         }
-
-    num_tokens = len(encoding.encode(text))
 
     return {"error": False, "message": "Token count successful", "data": num_tokens}
 
 
-# if __name__ == "__main__":
-#     # app.run(debug=True)
-#     from waitress import serve
+if __name__ == "__main__":
+    # app.run(debug=True)
+    from waitress import serve
 
-#     serve(app, host="127.0.0.1", port=5000)
+    load_dotenv()
+
+    # for m in genai.list_models():
+    #     if "generateContent" in m.supported_generation_methods:
+    #         print(m.name)
+    print("Server Started")
+    serve(app, host="127.0.0.1", port=5000)
 
 
 # from flask import Flask
